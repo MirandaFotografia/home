e3929f8af92a1f47fa4906b73514c12a966e7e72
ArturDeAndrade <adeandrade.42@gmail.com>
2020-11-17 00:02:29 -0300

    - Carrossel de fotos;\n- Adiçao de fotografias.

---
+++ index.js
@@ -0,0 +1,37 @@
+const carrossel = document.querySelector('#carrossel')
+let posicao = null
+let movendo = false
+let translacao = 0
+
+const inicio = e => {
+    posicao = e.pageX
+    movendo = true
+    const matriz = window.getComputedStyle(carrossel).getPropertyValue('transform')
+    if (matriz !== "none") {
+        translacao = parseInt(matriz.split(',')[4].trim())
+    }
+}
+
+const meio = e => {
+    if (movendo) {
+        const movimento = e.pageX - posicao
+        carrossel.style.transform = `translateX(${movimento + translacao}px)`
+    }
+}
+
+const fim = e => {
+    movendo = false
+}
+
+if (window.PointerEvent) {
+    window.addEventListener('pointerdown', inicio)
+    window.addEventListener('pointermove', meio)
+    window.addEventListener('pointerup', fim)
+} else {
+    window.addEventListener('mousedown', inicio)
+    window.addEventListener('mousemove', meio)
+    window.addEventListener('mouseup', fim)
+    window.addEventListener('touchstart', inicio)
+    window.addEventListener('touchmove', meio)
+    window.addEventListener('touchend', fim)
+}
