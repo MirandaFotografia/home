b8a8a8d2dd28bb5bfe38ba6c2eafbde2d8ff1a72
ArturDeAndrade <adeandrade.42@gmail.com>
2020-12-03 21:07:39 -0300

    Criaçao de margens para as fotos das seçoes 'Sobre Mim' e 'Feedbacks', reformulaçao do carrossel.

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
 // Carrossel de Fotos
-const carrossel = document.querySelector('#carrossel')
-let posicao = null
-let movendo = false
-let translacao = 0
+// const carrossel = document.querySelector('#carrossel')
+// let posicao = null
+// let movendo = false
+// let translacao = 0
 
-const inicio = e => {
-    posicao = e.clientX
-    movendo = true
-    const matriz = window.getComputedStyle(carrossel).getPropertyValue('transform')
-    if (matriz !== "none") {
-        translacao = parseInt(matriz.split(',')[4].trim())
+// const inicio = e => {
+//     posicao = e.clientX
+//     movendo = true
+//     const matriz = window.getComputedStyle(carrossel).getPropertyValue('transform')
+//     if (matriz !== "none") {
+//         translacao = parseInt(matriz.split(',')[4].trim())
+//     }
+// }
+
+// const meio = e => {
+//     if (movendo) {
+//         const movimento = e.clientX - posicao
+//         if (movimento + translacao < 0 && movimento + translacao > -1450) {
+//             carrossel.style.transform = `translateX(${movimento + translacao}px)`
+//         }
+//     }
+// }
+
+// const fim = e => {
+//     movendo = false
+// }
+
+// if (window.PointerEvent) {
+//     carrossel.addEventListener('pointerdown', inicio)
+//     carrossel.addEventListener('pointermove', meio)
+//     carrossel.addEventListener('pointerup', fim)
+// }
+
+const botaoEsq = document.querySelector('#voltar')
+const botaoDir = document.querySelector('#avancar')
+const imagens = document.querySelectorAll('.carrossel__imagem')
+let ind = 0
+imagens[ind].classList.toggle("selecionada")
+
+const voltar = evento => {
+    botaoDir.style.visibility = "initial"
+    imagens[ind].classList.toggle("selecionada")
+    ind--
+    imagens[ind].classList.toggle("selecionada")
+    if (ind === 0) {
+        botaoEsq.style.visibility = "hidden"
+    }
+}
+const avancar = evento => {
+    botaoEsq.style.visibility = "initial"
+    imagens[ind].classList.toggle("selecionada")
+    ind++
+    imagens[ind].classList.toggle("selecionada")
+    if (ind === 4) {
+        botaoDir.style.visibility = "hidden"
     }
 }
 
-const meio = e => {
-    if (movendo) {
-        const movimento = e.clientX - posicao
-        if (movimento + translacao < 0 && movimento + translacao > -1450) {
-            carrossel.style.transform = `translateX(${movimento + translacao}px)`
-        }
-    }
-}
-
-const fim = e => {
-    movendo = false
-}
-
-if (window.PointerEvent) {
-    carrossel.addEventListener('pointerdown', inicio)
-    carrossel.addEventListener('pointermove', meio)
-    carrossel.addEventListener('pointerup', fim)
-}
+botaoEsq.addEventListener('click', voltar)
+botaoDir.addEventListener('click', avancar)
